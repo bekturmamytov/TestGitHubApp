f4151befb6eaeba82e6490e4eddda7342d6e8dda
bekturmamytov <bekturmamytov@gmail.com>
2022-09-24 08:27:19 +0900

    Initial Commit

---
+++ TestGitHubApp/TestGitHubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitHubAppApp.swift
+//  TestGitHubApp
+//
+//  Created by Bektur Mamytov on 24/9/22.
+//
+
+import SwiftUI
+
+@main
+struct TestGitHubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
